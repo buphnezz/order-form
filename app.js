883252e122f9137d11f901b46289c5f924c721bf
zachary schumpert <buphnezz@gmail.com>
2018-01-25 14:16:27 -0800

    created dropdown, quantity input and Add to Cart buttons, also added a quantity input function in JS

---
+++ app.js
@@ -0,0 +1,15 @@
+// drop down menu in html
+// allow user to click each item from drop down
+// when item is selected from drop down display a prompt that accepts a number type input.
+// store that user number in an array
+// create a button called Add to Cart
+// allow user to select Add to Cart submit button 
+// store user input into a variable
+// clear the individual input numbers for each image but still keep the total number variable.
+// create item constructor with filepath and name  (use the constructor we already have).
+
+function itemQuantity() {
+  var numberOfItems = parseInt(prompt('Please input the desired quantity'));
+  console.log('Desired Quantity ' + itemQuantity);
+}
+
