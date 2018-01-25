ffb7393d11a0a7e70a416c8f4dfd639bfb88770e
zachary schumpert <buphnezz@gmail.com>
2018-01-25 15:09:18 -0800

    added function so when user clicks add to cart they receive prompt saying that items have been added to cart

--- app.js
+++ app.js
@@ -8,8 +8,48 @@
 // clear the individual input numbers for each image but still keep the total number variable.
 // create item constructor with filepath and name  (use the constructor we already have).
 
-function itemQuantity() {
-  var numberOfItems = parseInt(prompt('Please input the desired quantity'));
-  console.log('Desired Quantity ' + itemQuantity);
+// Item.allItems = [];
+
+// Item.numberOfEachItem = 0;
+// var itemName = [];
+// var itemQuantity = [];
+
+function itemsHaveBeenAdded() {
+  // Get the snackbar DIV
+  document.getElementById("add-to-cart").innerHTML = 'Items have been added to cart!';
+
 }
+
+
+
+// function Item(filePath, nameOfItem) {
+//   this.filePath = filePath;
+//   this.nameOfItem = nameOfItem;
+//   this. = avgCookiesSold;
+//   this.maxCustomers = maxCustomers;
+//   this.salesPerEachHour = [];
+//   this.totalCookies = 0;
+  
+
+// function addToCart(event) {
+//   console.log('add item to cart');
+//   event.preventDefault();
+//   console.log(event.target.storeName.value);
+//   var newStoreName = event.target.storeName.value;
+//   var newMinCustomers = event.target.minCustomers.value;
+//   var newMaxCustomers = event.target.maxCustomers.value;
+//   var newAvgCookiesSold = event.target.avgCookiesSold.value;
+
+//   new Store(newStoreName, newMinCustomers, newMaxCustomers, newAvgCookiesSold);
+
+//   storeTable.innerHTML = '';
+//   makeHeaderRow();
+//   renderAllStores();
+//   totalCookieFooter();
+// }
+
+// function itemQuantity() {
+//   var numberOfItems = parseInt(prompt('Please input the desired quantity'));
+//   console.log('Desired Quantity ' + itemQuantity);
+// }
 
