56ba41ad18bd2d0bf15a52c98c7a6dd1c7e210fd
zachary schumpert <buphnezz@gmail.com>
2018-01-25 17:13:58 -0800

    fixed our errors

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ function Product(filepath, name) {
   this.filepath = filepath;
   this.name = name;
 
-  Product.allProducts.push(this);
-  name.push(this.name);
+  // Product.allProducts.push(this);
+  // name.push(this.name);
 }
 
 new Product('img/bag.jpg', 'R2D2 BAG');
@@ -34,14 +34,15 @@ new Product('img/water-can.jpg', 'Water-Can');
 new Product ('img/wine-glass.jpg','Wine-Glass');
 
 
-var imgEl = document.getElementById('img');
-imgEl.src = Product.allProducts.filepath;
-imgEl.alt = Product.allProducts.name;
+// var imgEl = document.getElementById('img');
+// console.log(imgEl);
+// imgEl.src = Product.allProducts.filepath;
+// imgEl.alt = Product.allProducts.name;
 
 
 //When Button Process Order is pushed animated message appears "Order Submited"
 
-function submitAlert(); 
+// function submitAlert();
 
 // drop down menu in html
 // allow user to click each item from drop down
@@ -52,12 +53,17 @@ function submitAlert();
 // store user input into a variable
 // clear the individual input numbers for each image but still keep the total number variable.
 // create item constructor with filepath and name  (use the constructor we already have).
+var allItems = document.getElementById('allItems');
+
+allItems.addEventListener('submit', itemsHaveBeenAdded);
 
 
-function itemsHaveBeenAdded() {
-  var viewCartMessageText = 'Click here to view your cart!';
-  var messagePlusLinkDisplayed = viewCartMessageText.link("cart.html");
-  document.getElementById("webPage").innerHTML = messagePlusLinkDisplayed;
-  alert('Your items have been added to the cart successfully!');
-  document.getElementById("add-to-cart").innerHTML = 'Items added successfully. Click the link below to view your cart!';
+function itemsHaveBeenAdded(event) {
+  event.preventDefault();
+  console.log('log has been fired');
+  // var viewCartMessageText = 'Click here to view your cart!';
+  // var messagePlusLinkDisplayed = viewCartMessageText.link("cart.html");
+  // document.getElementById("webPage").innerHTML = messagePlusLinkDisplayed;
+  // alert('Your items have been added to the cart successfully!');
+  // document.getElementById("add-to-cart").innerHTML = 'Items added successfully. Click the link below to view your cart!';
   }
